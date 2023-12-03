e666a0eafd4e3dd355f96cb67a3e5f92392c4654
Sandra Dee <sandra.derbring@gmail.com>
2023-12-03 08:54:28 +0100

    1

--- 1.py
+++ 1.py
@@ -1,2 +1,48 @@
 #december 1
 
+def testData():
+
+    otest = open('test.txt', 'r')
+    test = otest.readlines()
+
+    oanswer = open('answer.txt', 'r')    
+    answer = oanswer.readline()
+
+    status = False
+    print("Runs test data")
+    result = runCode(test)
+
+    if result == int(answer): #not always int
+        status = True
+ 
+    print("Correct answer: " + answer + "My answer: " + str(result))
+    return status
+    
+
+def runCode(data):
+    print("Runs code")
+    
+    numbers = []
+
+    #a list of numbers
+    for line in data:
+        number = ''
+        for char in line:
+            if char.isdigit():
+                number+=char
+        numbers.append(number)
+
+    total = sum(numbers)
+    return total
+
+#Runs testdata
+testResult = testData()
+
+if testResult == True:
+    print("Test data parsed. Tries to run puzzle.")
+    opuzzle = open('input.txt', 'r')
+    puzzle = opuzzle.readlines()
+    finalResult = runCode(puzzle)
+    print(finalResult)
+else:
+    print("Test data failed. Code is not correct. Try again.")
